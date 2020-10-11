0125c378d207acac10b59a3b43e6359ac32e615d
christopherzhou <christopherjzhou@gmail.com>
2020-10-11 14:40:16 -0400

    Revert "bad comments"
    This reverts commit 291a1a722f239bfcb0189630290aa6d352686088.

--- data/test.py
+++ data/test.py
@@ -6,11 +6,5 @@ list = []
 for i in range(200000):
     list.append({i*200/3:'hello!'})
 
-    # bad comments:
-    # ajsldkfjaldskfja
-    # laksdjfklasdjfkl
-    # laksfjlkasjdfk
-    # alsdkfjalksdfj
-
 print(list)
 print("--- %s seconds ---" % (time.time() - start_time))
